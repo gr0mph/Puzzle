01abf1bc8eae9aa71cbd2ff37fbe658d14056ab6
gr0mph <danoupro@gmail.com>
2021-04-23 08:42:35 +0200

    Credit Card Verifier in Python

---
+++ puzzle/credit-card-verifier-luhns-algorithm/main.py
@@ -0,0 +1,11 @@
+# Thanks to MasterZydra
+# Starting like that:
+
+n = int(input())
+for i in range(n):
+    card = input()
+    card = card.replace(' ', '')[::-1]
+
+# Iterate through card than apply the compute.
+# Done !
+# Reverse list
